c9ce3084bdd1c5d127fcb8a6fce91d501b3b9a79
OlenaGermak <olenagermak1388@gmail.com>
2019-02-05 22:19:44 +0200

    initial commit

---
+++ test.js
@@ -0,0 +1,139 @@
+/*
+let veggitables =['tomato', 'potato']
+let fruits =['apple', 'orange']
+
+let salat= fruits.concat(veggitables);
+console.log(salat);
+let salatStr= salat. join(' ');
+console.log(salatStr);
+salat.sort();
+console.log(salat);
+salat.reverse();
+console.log(salat);
+
+
+let shift= salat.shift();
+console.log(shift);
+console.log(salat);
+
+let unshift= salat.unshift('egg', 'onion');
+console.log(unshift);
+console.log(salat);
+
+
+let pop = salat.pop();
+console.log(pop);
+console.log(salat);
+
+let push = salat.push('garlic','cucumber');
+console.log(push);
+console.log(salat);
+*/
+
+
+/*console.log(salat.slice(2));
+console.log(salat.slice(2,5));
+console.log(salat.slice(2,-2));*/
+
+/*
+let newSalat = salat.splice(3,1, 'chicken', 'mayonnaise' )
+console.log(salat);
+console.log(newSalat);
+
+
+let newSalat = salat.splice(3,0, 'chicken', 'mayonnaise' )
+console.log(salat);
+console.log(newSalat);*/
+
+
+
+/*
+let club = ['Arsenal', 'Liverpool', 'Man.City'];*/
+/*for (let i=0; i<=club.length; i++){
+    console.log(i, club[i]);
+}*///варіант перебирання масиву
+
+/*club.forEach(function(i, item, arr){
+   console.log(i, item, arr);          
+             })
+             
+             */
+             
+             
+/*             
+   for(let key in club){
+       console.log(key);
+   }   
+
+
+
+  for(let key in club){
+       console.log(key, club[key]);
+   } 
+         */    
+/*
+            
+let obj1 = new Object();          
+let obj2 = {};   
+let obj3 = {
+    userName:'Ivan',
+    userAge: 25,
+    userWork: false,
+    'date of birth': '01.01.1990'
+};  
+             
+let arr3=['Ivan', 25, false] ;
+
+    console.log(obj3);
+console.log(arr3); 
+console.log(arr3[0]); 
+console.log('obj3.userName', obj3.userName ); 
+console.log('obj3["userName"]', obj3['userName'] ); 
+ console.log('obj3["userName"]', obj3["date of birth"] );             
+             
+             
+             
+ obj1.name = 'Petro'            
+    console.log(obj1);
+obj3.city = 'Lviv'            
+    console.log(obj3);
+obj3.city = 'Stryi'            
+    console.log(obj3);
+*/
+
+/*             
+  delete obj3.city;           
+     console.log(obj3);  */      
+             
+  /*if('city' in obj3) {
+     console.log('yes');        
+              
+  }
+else{ console.log('no');  }*/
+
+/*const obj4 = {
+    name: 'Petro',
+    sname:'Petriv'
+}
+ console.log(obj4);
+obj4.name= 'Ivan';
+ console.log(obj4);*/
+             
+             
+   let obj5 = {
+    name: 'Petro',
+    sname:'Petriv',
+       birth:{
+           date:'01',
+           month:'01',
+           year:'1990'
+       }
+}
+ console.log(obj5);
+console.log(obj5.birth.year);
+          
+             
+
+
+
+
